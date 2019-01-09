f89a5df189e2c43f0538c1f8f66c7aa6757410a3
julius busecke <jbusecke@princeton.edu>
2019-01-09 16:59:52 -0500

    some upgrades mostly jupyterlab

--- jupyterlab/install_extensions.sh
+++ jupyterlab/install_extensions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 conda install -y -c conda-forge nodejs
-jupyter labextension install dask-labextension @pyviz/jupyterlab_pyviz
+jupyter labextension install dask-labextension @pyviz/jupyterlab_pyviz @jupyterlab/toc @jupyterlab/github @quentinandre/jupyterlab_snippets
 jupyter lab build
