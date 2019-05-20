4d3f88241f03f9d9e6bbdda79d941d8758adebea
julius busecke <jbusecke@princeton.edu>
2019-05-20 11:14:20 -0400

    gfdl changes ;

--- jupyterlab/install_extensions.sh
+++ jupyterlab/install_extensions.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 conda install -y -c conda-forge nodejs
-jupyter labextension install dask-labextension @krassowski/jupyterlab_go_to_definition @pyviz/jupyterlab_pyviz @jupyterlab/toc @jupyterlab/github @quentinandre/jupyterlab_snippets
+
+jupyter labextension install dask-labextension @krassowski/jupyterlab_go_to_definition @pyviz/jupyterlab_pyviz @jupyterlab/toc @jupyterlab/github @quentinandre/jupyterlab_snippets @ryantam626/jupyterlab_code_formatter @jupyterlab/git
+
+pip install jupyterlab-git
+pip install jupyterlab_snippets
+pip install jupyterlab_code_formatter
+
+jupyter serverextension enable --py jupyterlab_code_formatter
+jupyter serverextension enable --py jupyterlab_git
+
 jupyter lab build
