f7950598ba6ddff4f87f3b067dccc40d9b47e02d
Julius Busecke <jbusecke@princeton.edu>
2019-07-01 17:50:55 -0400

    Update install_extensions.sh

--- jupyterlab/install_extensions.sh
+++ jupyterlab/install_extensions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conda install -y -c conda-forge nodejs
+# conda install -y -c conda-forge nodejs # This should be present in the base conda env (from where you call jupyterlab)
 
 jupyter labextension install dask-labextension @krassowski/jupyterlab_go_to_definition @pyviz/jupyterlab_pyviz @jupyterlab/toc @jupyterlab/github @ryantam626/jupyterlab_code_formatter @jupyterlab/git
 
