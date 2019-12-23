4e55333591a26feaaf7e9c6a99fca13a9b5e4c20
jbusecke <julius@ldeo.columbia.edu>
2019-12-22 22:56:44 -0500

    added things and some other minor edits

--- mac_setup/install_app_store.sh
+++ mac_setup/install_app_store.sh
@@ -11,7 +11,7 @@ mas install 409203825 #Numbers (6.1)
 mas install 1435957248 #Drafts (14.2)
 mas install 775737590 #iA Writer (5.2.8)
 mas install 425264550 #Disk Speed Test (3.2)
-mas install 1147396723 #WhatsApp (0.3.4375)
 mas install 425424353 #The Unarchiver (3.11.1)
 mas install 961632517 #Be Focused Pro (1.7.5)
 mas install 926036361  #LastPass Password Manager
+mas install 904280696  #Things 3 (3.11)
--- mac_setup/install_brew.sh
+++ mac_setup/install_brew.sh
@@ -30,10 +30,12 @@ brew cask install spotify
 # brew cask install mactex
 brew cask install disk-inventory-x
 brew cask install zotero
-brew cask install omnifocus
+# brew cask install omnifocus
 brew cask install google-backup-and-sync
 # brew cask install adobe-creative-cloud
 # brew cask install hazel
+brew install carbon-copy-cloner
+
 
 # these will ask for password
 brew cask install homebrew/cask-drivers/sony-ps4-remote-play homebrew/cask-drivers/qnap-external-raid-manager
