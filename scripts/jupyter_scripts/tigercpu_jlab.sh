33ad2c095697c1857f01b3a45d18a28f850e699d
Julius Busecke <jbusecke@princeton.edu>
2020-05-05 10:05:56 -0400

    some updates to the config files

--- scripts/jupyter_scripts/tigercpu_jlab.sh
+++ scripts/jupyter_scripts/tigercpu_jlab.sh
@@ -12,11 +12,16 @@ dask_port=10002
 
 echo -e "
 Command to create ssh tunnel:
-ssh -N -L ${port}:${node}:${port} -L ${dask_port}:${node}:${dask_port} tigercpu
+ssh -N -L ${port}:${node}:${port} -L ${dask_port}:${node}:9999 tigercpu
 
 Use a Browser on your local machine to go to:
 localhost:${port}  (prefix w/ https:// if using password)
 "
-
+#experimental 
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+# from https://docs.dask.org/en/latest/array-best-practices.html
+echo "Check if these experimental changes fuck smth up"
 
 jupyter-lab --no-browser --port=${port} --ip=0.0.0.0
